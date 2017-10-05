87074d15bfc1e74a7695099171f70d0a2e64c5e5
Troy Maynard <troyvmaynard@gmail.com>
2017-10-05 16:39:17 -0400

    Second commit, deleted old project and clones the steam tutorial modded so I can break it 1 piece at a time.

--- Prosfair_02/Source/Prosfair_02/Prosfair_02.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Prosfair_02.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Prosfair_02, "Prosfair_02" );
